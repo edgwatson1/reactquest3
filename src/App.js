4a641708b8ac1833d968e2d06f14d9cf7bb3e4bf
@e17 <e@edwardwatson.com>
2019-10-22 17:40:03 +0100

    updatereact3quest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quote from "./quotes.js";
+
+class App extends Component {
+  state = { working: true };
+
+  changeClass = () => {
+    this.setState({
+      working: !this.state.working
+    });
+  };
+
+  changeClassString = () => {
+    const className = this.state.working === true ? "working" : "notworking";
+    return className;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className={this.changeClassString()}>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+
+          <button
+            class="ui active button yellow"
+            style={{ margintop: "10px" }}
+            onClick={this.changeClass}
+          >
+            <i class="user icon"></i>
+            Click to change whether Homer is working
+          </button>
+        </header>
+
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
